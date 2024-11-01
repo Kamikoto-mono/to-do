af6dddbdb31ad558fbb53e621a667a951a1181ce
Daniil Dotsenko <kamikoto_mono@mail.ru>
2024-11-01 13:17:11 +0300

    deleted the excess

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { configConnectionPostgres } from './database/connectionDatabase';
-import { TasksModule } from './tasks/tasks.module';
-import Task from './database/entity/task';
 
 
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(configConnectionPostgres),
-    TasksModule, 
   ],
   controllers: [AppController],
   providers: [AppService],
